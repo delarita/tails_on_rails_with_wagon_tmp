73d2ad8f7a4c09f9dd3705a904d5c87fd541495c
delarita <j.delarita@gmail.com>
2021-06-22 12:50:37 +0200

    posts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
+//import Rails from "@rails/ujs"
 //import Turbolinks from "turbolinks"
 import * as Turbo from "@hotwired/turbo"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-Rails.start()
-Turbo.start()
+//Rails.start()
+//Turbo.start()
 ActiveStorage.start()
 
 
